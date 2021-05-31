690c8ca03026c8c31d2020a3fde9dfebc901a8a6
unknown <dmsdl950823@gmail.com>
2021-05-31 23:41:42 +0900

    Updated Clone coding for FullStack

--- 210526_Full Stack Clone Coding Garden/client/client.js
+++ 210526_Full Stack Clone Coding Garden/client/client.js
@@ -5,10 +5,12 @@
 const form = document.querySelector('form') // <form>
 const loadingElement = document.querySelector('.loading') // <div.loading>
 
+const mewsElement = document.querySelector('.mews')
 const API_URL = 'http://localhost:5000/mews' // API 가져올/보낼 주소 => 처음에 CORS 에러!
 
 loadingElement.style.display = 'none'
 
+listAllMews()
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -25,13 +27,54 @@ form.addEventListener('submit', e => {
 
     // posting
     fetch(API_URL, {
+         // 1) Send POST request
         method: 'POST',
         body: JSON.stringify(mew),
         headers: {
             'content-type': 'application/json'
         }
-    }).then(response => response.json())
-      .then(createdMeow => {
-          console.log(createdMeow) // 보낸 데이터
-      })
+    })
+    .then(response => response.json())
+    .then(createdMeow => { // 5) Get response from server
+        console.log(createdMeow)
+
+        form.reset()
+        setTimeout(() => {
+            form.style.display = ''
+        }, 3000)
+
+        listAllMews() // 6) Get all lists from server
+    })
 })
+
+function listAllMews () {
+    mewsElement.innerHTML = ''
+    
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(lists => {
+            console.log(lists) // all lists from database
+
+            lists.reverse()
+            lists.forEach(item => {
+                const div = document.createElement('div')
+
+                const header = document.createElement('h3')
+                header.textContent = item.name
+
+                const contents = document.createElement('p')
+                contents.textContent = item.content
+                
+                const date = document.createElement('small')
+                date.textContent = new Date(item.created)
+
+                div.appendChild(header)
+                div.appendChild(contents)
+                div.appendChild(date)
+
+                mewsElement.appendChild(div)
+            })
+
+            loadingElement.style.display = 'none'
+        })
+}
--- 210526_Full Stack Clone Coding Garden/server/server.js
+++ 210526_Full Stack Clone Coding Garden/server/server.js
@@ -8,27 +8,66 @@ const app = express()
 
 app.use(cors())  // 모든 Access-Control Error 를 차단함
 app.use(express.json())
-const db = monk('localhost/meower')
+const db = monk('localhost/meower') // mewoer 라는 db 생성됨
+const lists = db.get('mews')
 
 // root directory :: http://localhost:5000/
 app.get('/', (req, res) => {
-    // res.json({ // json
-    //     message: 'Meower! 😸'
-    // })
+    // json
+    // res.json({ message: 'Meower! 😸' })
     // res.send('Hello World! MEW! 😸 WELCOME CATOS!') // string
     res.send('<h1>Hello World! MEW! 😸 WELCOME CATOS!<br>This is Cato\'s home!</h1>') // html
 })
 
-// 📥 GET data from DB
+// 📥 4) GET all data from DB :: http://localhost:5000/mews
 app.get('/mews', (req, res) => {
+    lists.find()
+        .then(mews => {
+            res.json(mews)
+        })
 }) 
 
 // 📤POST data
 // const API_URL = 'http://localhost:5000/mews' // Client API :: 가져올/보낼 주소 => 처음에 CORS 에러!
 app.post('/mews', (req, res) => {
+    // 2) get body from client's request (you can see the result through command line)
+    console.log(req.body)
+
+    // 3) response to client
+    if (isValidMew(req.body)) {
+        console.log('passed 🤩')
+
+        const payload = {
+            name: req.body.name.toString(),
+            content: req.body.content.toString(),
+            created: new Date()
+        }
+        
+        // 3-1) insert into db...
+        lists.insert(payload)
+            .then(insertedData => {
+                res.json(insertedData)
+            })
+
+    } else {
+        console.log('unpassed 😒')
+
+        // 3-2) Error Handling
+        response.status(422)
+        res.json({
+            message: 'Hey! Name and Content are required!'
+        })
+    }
 })
 
 
 app.listen(5000, () => {
     console.log('Listening on http://localhost:5000')
 })
+
+function isValidMew (mew) {
+    return (
+        mew.name && mew.name.toString().trim() !== '' &&
+        mew.content && mew.content.toString().trim() !== ''
+    )
+}
