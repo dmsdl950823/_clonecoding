44874b36d04b87453e3862d01ab1ee3037ea89e5
unknown <dmsdl950823@gmail.com>
2021-06-08 23:23:44 +0900

    Fixed Fonder's name & Creating React Component

---
+++ 210607_React Hooks + MERN/expense-tracker-react/src/components/Balance.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Balance = () => {
+  return (
+    <h4>Your Balance</h4>
+  )
+}
---
+++ 210607_React Hooks + MERN/expense-tracker-react/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+// eslint 설정
+// npx eslint --init
+// ./node_modules/.bin/eslint --init
+
+ReactDOM.render(<App />, document.getElementById('root'))
