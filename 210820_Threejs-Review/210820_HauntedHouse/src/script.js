e2857539d1db68c6c453a92880b401ca9ae90502
dmsdl950823 <dmsdl950823@gmail.com>
2021-08-22 00:43:48 +0900

    Using diverse methods of three.js

--- 210820_Threejs-Review/210820_HauntedHouse/src/script.js
+++ 210820_Threejs-Review/210820_HauntedHouse/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 /**
  * Base
@@ -21,17 +22,26 @@ const scene = new THREE.Scene()
  * House
  */
 // Temporary sphere
-// const sphere = new THREE.Mesh({
-
-// })
-// scene.add(sphere)
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshPhysicalMaterial({ color: '#B0FFF8' })
+const box = new THREE.Mesh(geometry, material)
+scene.add(box)
 
 // Floor
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial({ color: '#ECFFB0' })
+)
+floor.rotation.x = - Math.PI * 0.5
+floor.position.y = 0
+scene.add(floor)
 
 /**
  * Lights
  */
 // Ambient Light
+const ambientLight = new THREE.AmbientLight('#FFFFFF', 0.5)
+scene.add(ambientLight)
 
 // Directional Light
 
@@ -45,24 +55,62 @@ const sizes = {
 
 window.addEventListener('resize', () => {
   // Update Sizes
+  sizes.width = window.innerWidth
+  sizes.height =  window.innerHeight
 
   // Update Camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
   // Update Renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
  * Camera
+ * PerspectiveCamera
+ * @param {Number} fov
+ * @param {Number} aspect 
+ * @param {Number} near 
+ * @param {Number} far
  */
-// Base Camera
-const camera = new THREE.PerspectiveCamera()
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 2
+camera.position.z = 4
+scene.add(camera)
 
 // Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.addEventListener('change', () => console.log('controling...'))
 
 /**
  * Renderer
  */
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
+const clock = new THREE.Clock()
+
+const tick = () => {
+
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update Controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
