d95cbe8ab9b5c0fdda2c248e6b53a037033955f5
dmsdl950823 <dmsdl950823@gmail.com>
2021-08-30 07:09:46 +0900

    Updated scripts for Three.js Review

--- 210820_Threejs-Review/210820_HauntedHouse/src/script.js
+++ 210820_Threejs-Review/210820_HauntedHouse/src/script.js
@@ -1,11 +1,15 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
+
+// 🎇 https://sbcode.net/threejs/
 
 /**
  * Base
  */
 // Debug
+const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -17,22 +21,52 @@ const scene = new THREE.Scene()
 /**
  * Textures
  */
+const textureLoader = new THREE.TextureLoader()
+
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const dooNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+const bricksColorTexture = textureLoader.load('/textures/bricks/color.jpg')
+const bricksAmbientOcclusionTexture = textureLoader.load('/textures/bricks/ambientOcclusion.jpg')
+const bricksNormalTexture = textureLoader.load('/textures/bricks/normal.jpg')
+const bricksRoughnessTexture = textureLoader.load('/textures/bricks/roughness.jpg')
+
+const grassColorTexture = textureLoader.load('/textures/grass/color.jpg')
+const grassAmbientOcclusionTexture = textureLoader.load('/textures/grass/ambientOcclusion.jpg')
+const grassNormalTexture = textureLoader.load('/textures/grass/normal.jpg')
+const grassRoughnessTexture = textureLoader.load('/textures/grass/roughness.jpg')
 
 /**
  * House
  */
-// Temporary sphere
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshPhysicalMaterial({ color: '#B0FFF8' })
-const box = new THREE.Mesh(geometry, material)
-scene.add(box)
+// House container
+const house = new THREE.Group()
+scene.add(house)
+
+// Walls
+const walls = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({
+    map: bricksColorTexture,
+    aoMap: bricksAmbientOcclusionTexture,
+    normalMap: bricksNormalTexture,
+    roughnessMap: bricksRoughnessTexture
+  })
+)
+walls.castShadow = true
+scene.add(walls)
 
 // Floor
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(20, 20),
   new THREE.MeshStandardMaterial({ color: '#ECFFB0' })
 )
-floor.rotation.x = - Math.PI * 0.5
+floor.rotation.x  = - Math.PI * 0.5
 floor.position.y = 0
 scene.add(floor)
 
@@ -41,10 +75,10 @@ scene.add(floor)
  */
 // Ambient Light :: This light globally illuminates all objects in the scene equally.
 const ambientLight = new THREE.AmbientLight('#FFFFFF', 0.5)
-// scene.add(ambientLight)
+scene.add(ambientLight)
 
 // Directional Light
-const directionalLight = new THREE.DirectionalLight('#FFFFFF', 0.9)
+const directionalLight = new THREE.DirectionalLight('#FFFFFF', 0.5)
 directionalLight.castShadow = true
 directionalLight.position.x = 3
 directionalLight.position.y = 6
@@ -53,6 +87,10 @@ const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight
 
 scene.add(directionalLight)
 scene.add(directionalLightHelper)
+
+gui.add(directionalLight, 'intensity', 0, 1, 0.01)
+// gui.add(directionalLight, 'intensity').min(0).max(1).step(0.01)
+
 
 /**
  * Sizes
@@ -76,6 +114,7 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+
 /**
  * Camera
  * PerspectiveCamera
@@ -90,6 +129,11 @@ camera.position.y = 2
 camera.position.z = 4
 scene.add(camera)
 
+const cameraGui = gui.addFolder('Camera')
+cameraGui.add(camera.position, 'x', -100, 100, 0.01)
+cameraGui.add(camera.position, 'y', -100, 100, 0.01)
+cameraGui.add(camera.position, 'z', -100, 100, 0.01)
+
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
@@ -99,6 +143,9 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.shadowMap.enabled = true // set Shadow
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
