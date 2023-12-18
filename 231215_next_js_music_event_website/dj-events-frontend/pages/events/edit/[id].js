72fa5cc8e4e617d4a97b3bffdc2d547e94ea3e7b
dmsdl950823 <dmsdl950823@gmail.com>
2023-12-18 15:15:45 +0900

    Added # pagination 추가

--- 231215_next_js_music_event_website/dj-events-frontend/pages/events/edit/[id].js
+++ 231215_next_js_music_event_website/dj-events-frontend/pages/events/edit/[id].js
@@ -11,11 +11,14 @@ import { API_URL } from '@/config/index'
 import style from '@/styles/Form.module.css'
 
 import Layout from '@/components/Layout'
+import Modal from '@/components/Modal'
+import ImageUpload from '@/components/ImageUpload'
 
 export default function EditEventPage({ evt }) {
   const [values, setValues] = useState({ ...evt })
 
   const [imagePreview, setImagePreview] = useState(evt.image ? evt.image.formats.thumbnail.url : null)
+  const [showModal, setShowModal] = useState(false)
 
   const router = useRouter()
 
@@ -49,6 +52,15 @@ export default function EditEventPage({ evt }) {
     const slug = values.name?.replace(/ /gi, '-').toLowerCase()
     setValues({...values, [name]: value, slug })
     // console.log('change', values)
+  }
+
+  const imageUploaded = async e => {
+    const res = await fetch(`${API_URL}/events/${evt.id}`)
+    const data = await res.json()
+    setImagePreview(data.image.formats.thumbnail.url)
+    setShowModal(false)
+
+    console.log(data);
   }
 
 
@@ -101,7 +113,11 @@ export default function EditEventPage({ evt }) {
         : (<div><p>No image Uploaded</p></div>)
       }
 
-      <div><button class="btn-secondary"><FaImage /> Set Image </button></div>
+      <div><button className="btn-secondary" onClick={e => setShowModal(true)}><FaImage /> Set Image </button></div>
+
+      <Modal show={showModal} onClose={() => setShowModal(false)}>
+        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
+      </Modal>
     </Layout>
   )
 }
--- 231215_next_js_music_event_website/dj-events-frontend/pages/events/index.js
+++ 231215_next_js_music_event_website/dj-events-frontend/pages/events/index.js
@@ -1,11 +1,12 @@
 import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem'
-import { API_URL } from '@/config/index'
+import Pagination from '@/components/Pagination'
+import { API_URL, PER_PAGE } from '@/config/index'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total }) {
   // console.log(events)
 
   return (
@@ -15,18 +16,23 @@ export default function EventsPage({ events }) {
       { events.length === 0 && <h3>No Events to show</h3> }
       { events.map(evt => (<EventItem key={evt.slug} evt={evt}></EventItem>)) }
       
-
+      <Pagination page={page} total={total}/>
     </Layout>
   )
 }
 
-export async function getStaticProps () {
-  const res = await fetch(`${API_URL}/api/event`)
-  const { data } = await res.json()
-  const events = data.map(({ id, attributes }) => ({ id, ...attributes }))
+export async function getServerSideProps ({ query: { page = 1 } }) {
+  // Calculate Start Page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE
+
+  // Fetch Events
+  const eventRes = await fetch(`${API_URL}/api/event?sort=date:desc&pagination[page]=${start}&pagination[pageSize]=${PER_PAGE}`)
+  const response = await eventRes.json()
+  
+  const events = response.data.map(({ id, attributes }) => ({ id, ...attributes }))
+  const total = response.meta.pagination.total
 
   return {
-    props: { events },
-    revalidate: 1
+    props: { events, page: +page, total }
   }
 }
